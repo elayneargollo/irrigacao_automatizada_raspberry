5f7719bbdd4dc692ccb9644dd474c1879a2f337c
Elayne <elayne.natalia@outlook.com>
2022-05-31 21:10:47 -0300

    log

--- README.md
+++ README.md
@@ -4,8 +4,14 @@ Projeto final de conclusão de curso ADS-IFBA
 
 ## Conteúdo
 
+- [Tecnologia Utilizada](#tecnologia)
 - [Pré-requisitos](#pré-requisitos)
 - [Execução Aplicação](#execução)
+- [Escopo](#escopo)
+
+## Tecnologia Utilizada
+
+- Biblioteca Prometheus_client
 
 ## Pré-requisitos
 
@@ -14,16 +20,57 @@ Estas são as instalações e configurações necessárias para executar o proje
 Para executar este projeto é necessário instalar:
 
 - Módulo Adafruit-ADS1x15 
+- Obter o projeto do back-end e front-end
 
 1. Após a instalação: 
 
    1.1 Habilite o I2C
 
-        - Abra o termial, digite raspi-config -> nterfacing Options -> I2C.
+        - Abra o termial, digite raspi-config -> interfacing Options -> I2C.
+        - Reinicie o Raspberry Pi
 
 ## Execução Aplicação
 
-    - python raspberryPi.py
+1. Clonar repositório git utilizando o comando:
+
+        git clone git@github.com:elayneargollo/tcc-raspberry.git
+
+2. Vá ate a pasta do projeto
+
+        cd tcc-raspberryPi
+
+3. Execute o programa
+
+        python raspberryPi.py
+
+Após a execução a inicialização conseguirá acessar:
+
+        - Logger de execução na raiz do projeto: irrigacao.log
+
+## Escopo
+
+A partir do cadastrado de plantas através de uma aplicação frontend, o raspberry irá acessar o banco de dados compartilhado com a aplicação de backend para atualizar os dados de sensores de acordo com os valores lidos em campo.
+
+- RF1. Calibrar Sensor.
+
+    - Cada sensor terá seu valor de calibração baseado nos valores mínimos e máximos em ambiente de solo completamente seco e completamente úmido.
+        - Calibrar sensor para solo seco e guardar o log dos valores obtidos.
+        - Calibrar sensor para solo úmido e guardar o log dos valores obtidos.
+
+- RF2. Obter Concentração Mínima do solo
+
+    - A partir do tipo de solo cadastrado na planta que está sendo monitorada pelo respectivo sensor, deve-se obter a concentração mínima de água necessária para manter a raiz saudável.
+    - Este valor deve está entra a percentagem máxima e mínima (100% e 0%).
+    - Cada solo téra seu valor de concentração.
+    - Cada solo deve ter seu peso seco e seu peso úmido persistido.
+    - Cada solo deve está associado a uma planta, um sensor e uma solenóide.
+
+- RF3. Irrigar Automáticamente
+
+    - Com os valores adquiridos após calibragem e concentração mínima deve-se irrigar automaticamente a planta se e somente se a valor lido pelo sensor for menor ou igual a concentração mínima.
+    - Ligar ou desligar a solenóide de acordo com o valor lido.
+    - Atualizar os dados do sensor.
+    - Aguardar até a próxima leitura.
 
 ### Autora
 
--- raspberryPi.py
+++ raspberryPi.py
@@ -1,6 +1,14 @@
+##
+##                 Autora: Elayne Natália de Oliveira de Morais
+## Para disciplina TCC - Instituto Federal da Bahia - Análise e Desenvolvimento de Sistemas
+## Monitoramente da umidade de solo, irrigação automática de plantas e calibração de sensor de umidade
+##                          Orientador: Manoel Neto
+##
+
 import adafruit_ads1x15.ads1115 as ADS
 import RPi.GPIO as GPIO
 from adafruit_ads1x15.analog_in import AnalogIn
+from prometheus_client import Gauge, start_http_server
 import board
 import busio
 import time
@@ -16,13 +24,23 @@ GPIO.setmode(GPIO.BCM)
 solenoide = 17
 GPIO.setup(solenoide, GPIO.OUT, initial=GPIO.LOW)
 
-sqliteConnection = sqlite3.connect('/home/elaynemorais/Documentos/tcc-back/app/main/banco.db');
+gh = Gauge('dht22_humidity_percent', 'Humidity percentage measured by the DHT22 Sensor')
+gt = Gauge('dht22_temperature', 'Temperature measured by the DHT22 Sensor', ['scale'])
+
+gt.labels('celsius')
+gt.labels('fahrenheit')
+
+NOME_ARQUIVO_LOGGER = 'irrigacao4.log'
+CONECTION_DEFAULT = '/home/elaynemorais/Documentos/tcc-back/app/main/banco.db'
+PORT = 8000
+
+sqliteConnection = sqlite3.connect(CONECTION_DEFAULT)
 
 def logger():
-    logging.basicConfig(filename="irrigacao.log", level=logging.DEBUG)
+    logging.basicConfig(filename=NOME_ARQUIVO_LOGGER, level=logging.DEBUG)
     
     logging.basicConfig(
-        filename="irrigacao.log",
+        filename=NOME_ARQUIVO_LOGGER,
         level=logging.DEBUG,
         format="%(asctime)s :: %(levelno)s :: %(lineon)d")    
     
@@ -36,26 +54,24 @@ def calcularUmidade(quantidade_amostra, sensor):
         tensao = sensor.voltage
 
         logging.info("{} - Amostra {} | Leitura: {} | Tensão {}".format(datetime.datetime.now(), start, leitura_sensor, tensao))
-        time.sleep(2)
+        time.sleep(10)
         
     media = somatorio/quantidade_amostra
     logging.info("{} - Media {}".format(datetime.datetime.now(), media))
-
-    print("media: {}".format(media))
 
 def obterValorMapeado(leitura_sensor, VALOR_MINIMO, VALOR_MAXIMO, PercentMin, PercentMax):
     return (leitura_sensor - VALOR_MINIMO) * (PercentMax - PercentMin) / (VALOR_MAXIMO - VALOR_MINIMO) + PercentMin
 
 def monitoramento():
-    sensores = obterSensores();
+    sensores = GetAllSensores()
     dateAndTime = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
     for sensor in sensores:
         pesoSoloUmido = sensor[8]
         pesoSoloSeco = sensor[7]
         concentracao = sensor[10]
-        VALOR_MINIMO = sensor[2]
-        VALOR_MAXIMO = sensor[3]
+        VALOR_MINIMO = 17381 #sensor[2] umido
+        VALOR_MAXIMO = 30766 #sensor[3]seco
         tag = sensor[1]
 
         logging.info("{} pesoSoloUmido: {} | pesoSoloSeco: {} | concentracao: {} | VALOR_MINIMO: {} | VALOR_MAXIMO: {} | tag: {}".format(datetime.datetime.now(), pesoSoloUmido, pesoSoloSeco, concentracao, VALOR_MINIMO, VALOR_MAXIMO, tag))
@@ -66,33 +82,38 @@ def monitoramento():
         leitura_sensor = sensor1.value    
         leitura_sensor = obterValorMapeado(leitura_sensor, VALOR_MINIMO, VALOR_MAXIMO, 100, 0)
 
-        logging.info("{} VALOR_MAXIMO: {} | VALOR_MINIMO: {} | CONCENTRACAO_MINIMA: {} | LEITURA_SENSOR: {}".format(datetime.datetime.now(), VALOR_MAXIMO, VALOR_MINIMO, concentracao, leitura_sensor))
+        logging.info("{} VALOR_MAXIMO: {} | VALOR_MINIMO: {} | CONCENTRACAO_MINIMA: {} | LEITURA_SENSOR: {}".format(datetime.datetime.now(), VALOR_MAXIMO, VALOR_MINIMO, concentracao, leitura_sensor))        
+        gh.set(leitura_sensor)
+        gt.labels('celsius').set(leitura_sensor)
 
-        if(leitura_sensor <= concentracao):
-            situacao = "IRRIGANDO"
+        irrigacaoAutomatica(dateAndTime,tag, concentracao, leitura_sensor)
 
-            logging.info(f"SITUACAO {dateAndTime}: {situacao}")
-            GPIO.output(solenoide, GPIO.LOW)
-            time.sleep(2)
-
-            logging.info(f"STATUS {dateAndTime}: SOLENÓIDE LIBERA FLUXO DE AGUA | ESPERA DEZ SEGUNDOS PARA PRÓXIMA LEITURA")
-
-            update(dateAndTime, "ABERTO", tag)
-
-        else:
-            situacao = "IRRIGADO"
-
-            logging.info(f"SITUACAO {dateAndTime}: {situacao}")
-            GPIO.output(solenoide, GPIO.HIGH)
-            time.sleep(2)
-
-            logging.info(f"STATUS {dateAndTime}: SOLENÓIDE CORTA FLUXO DE AGUA | ESPERA DEZ SEGUNDOS PARA PRÓXIMA LEITURA")
-            update(dateAndTime, "FECHADO", tag)
-
-    logging.info("ESPERA DEZ SEGUNDOS PARA PRÓXIMA LEITURA")
     time.sleep(10)
 
-def obterSensores():
+# Funçao que de acordo com o valor de concentracao e leitura do sensor, irriga automaticamente
+def irrigacaoAutomatica(dateAndTime,tag, concentracao, leitura_sensor):
+    
+    if(leitura_sensor <= concentracao):
+
+        GPIO.output(solenoide, GPIO.LOW)
+        time.sleep(1)
+
+        logging.info(f"STATUS {dateAndTime}: SOLENÓIDE LIBERA FLUXO DE AGUA | ESPERA DEZ SEGUNDOS PARA PRÓXIMA LEITURA")
+        update(dateAndTime, "ABERTO", tag, concentracao)
+
+    else:
+        
+        GPIO.output(solenoide, GPIO.HIGH)
+        time.sleep(1)
+
+        logging.info(f"STATUS {dateAndTime}: SOLENÓIDE CORTA FLUXO DE AGUA | ESPERA DEZ SEGUNDOS PARA PRÓXIMA LEITURA")
+        update(dateAndTime, "FECHADO", tag, concentracao)
+
+## Função que obtém todos os sensores cadastrados no banco de dados relacionado as suas respectivas plantas e solo
+def GetAllSensores():
+    
+    logging.info(f"Método GetAllSensores")
+
     try:
         curs=sqliteConnection.cursor()
         
@@ -102,19 +123,17 @@ def obterSensores():
                                     valorCalibracaoMinimo, 
                                     valorCalibracaoMaximo, 
                                     solenoideId,
-                                    p.nome, p.plantaId, 
-                                    so.pesoSoloSeco, so.pesoSoloUmido, so.quantidadeAmostra, so.concentracaoMinima, so.soloId
+                                    planta.nome, planta.plantaId, 
+                                    solo.pesoSoloSeco, solo.pesoSoloUmido, solo.quantidadeAmostra, solo.concentracaoMinima, solo.soloId
                                 from 
-                                    sensores as s
-                                INNER JOIN plantas as p ON p.plantaId = s.plantaId
-                                INNER JOIN solos as so ON p.tipoSoloId = so.soloId"""
+                                    sensores as sensor
+                                INNER JOIN plantas as planta ON planta.plantaId = sensor.plantaId
+                                INNER JOIN solos as solo ON planta.tipoSoloId = solo.soloId"""
                               
         curs.execute(sqlite_select_query)
         records = curs.fetchall()
 
-        print("Total de sensores encontrados:  ", len(records))
-        print("Sensores encontrados:  ", (records))
-        print("Exibindo sensores")
+        logging.info(f"Total de sensores encontrados: {len(records)}")
 
         for row in records:
             print("======================")
@@ -139,16 +158,18 @@ def obterSensores():
     except Exception as e:
         print(e)
 
-def update(dateAndTime, status, tag):
+## Função que atualiza os valores do sensor de acordo com a última leitura realizada pelo sistema
+def update(dateAndTime, status, tag, concentracao):
+
     logging.info(f"{dateAndTime} Atualizar sensor tag: {tag} no banco de dados com status: {status}")
 
     try:
         curs=sqliteConnection.cursor()
 
-        sqlite_update_query = """Update SENSORES set status = ?, dataLeitura = ? where tag = ?"""
+        query = """Update SENSORES set status = ?, dataLeitura = ? where tag = ?"""
         columnValues = (status,datetime.datetime.now(),tag)
 
-        curs.execute(sqlite_update_query, columnValues)
+        curs.execute(query, columnValues)
 
         sqliteConnection.commit()
         curs.close()
@@ -157,10 +178,12 @@ def update(dateAndTime, status, tag):
         print(e)
 
 def init():
+    metrics_port = PORT
+    start_http_server(metrics_port)
+
     while (True):
         logger()   
         monitoramento()
-        #monitoramento(145, 190, 30766, 17381)
-        #calcularUmidade(1000, canal0)
+        #calcularUmidade(1000, sensor1)
  
 init()
